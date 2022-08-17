87e6e563b5bc139aa76e52ebdbfd91bc8ecb9da7
Leo.Ji <jirenshi@meifute.com>
2022-08-17 13:20:44 +0800

    feat: 添加类组件

--- src/comp/user/index.jsx
+++ src/comp/user/index.jsx
@@ -1,34 +1,43 @@
 import React from 'react';
 import Page from '@common/Page';
 
-export default Page(({ history }) => {
-  return (
-    <div className="comp-user">
-      <div className="title">个人中心</div>
-      <div className="nick">你的昵称是：张三</div>
-      <a
-        onClick={() => {
-          history.back();
-        }}
-      >
-        返回
-      </a>
-      <br />
-      <a
-        onClick={() => {
-          history.push('/');
-        }}
-      >
-        返回首页
-      </a>
-      <br />
-      <a
-        onClick={() => {
-          history.push('/activity');
-        }}
-      >
-        活动中心
-      </a>
-    </div>
-  );
-});
+@Page
+export default class User extends React.Component {
+  componentDidMount() {
+    console.log('user componentDidMount');
+  }
+
+  render() {
+    const { history } = this.props;
+
+    return (
+      <div className="comp-user">
+        <div className="title">个人中心</div>
+        <div className="nick">你的昵称是：张三</div>
+        <a
+          onClick={() => {
+            history.back();
+          }}
+        >
+          返回
+        </a>
+        <br />
+        <a
+          onClick={() => {
+            history.push('/');
+          }}
+        >
+          返回首页
+        </a>
+        <br />
+        <a
+          onClick={() => {
+            history.push('/activity');
+          }}
+        >
+          活动中心
+        </a>
+      </div>
+    );
+  }
+}
